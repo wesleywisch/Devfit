4e70535c13e7efa0ebc1d8150d626b3ed69c50dc
wesley_wisch <wesley_wisch@hotmail.com>
2021-10-07 11:07:15 -0300

    Componente dos meses, dias e o status e a legenda da tela home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,78 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { HomeContainer } from './styles.js';
+import { connect } from 'react-redux';
 
-export function Home() {
+import {
+  HomeContainer,
+  Legend,
+  LegendText,
+  LegendItem,
+  LegendBox,
+  ConfigButtonArea,
+  ConfigButtonImage,
+} from './styles';
+
+import { HomeMonthScroll } from '../../components/HomeMonthScroll';
+import { HomeDaysScroll } from '../../components/HomeDaysScroll';
+import { HomeDayStatus } from '../../components/HomeDayStatus';
+
+function HomePage(props) {
+  let legends = [
+    { name: 'Hoje', color: '#b5eeff' },
+    { name: 'Treino feito', color: '#b5ffb8' },
+    { name: 'Treino perdido', color: '#ffb5b5' },
+    { name: 'dia de descanso', color: '#c5c5c5', opacity: 0.2 },
+    { name: 'Dia futuro', color: '#ccc' },
+  ];
+
   return (
     <HomeContainer>
-      <Text>TELA HOME</Text>
+      <HomeMonthScroll />
+      <HomeDaysScroll />
+      <HomeDayStatus />
+
+      <Legend>
+        <LegendText>Legenda:</LegendText>
+        {legends.map((item, key) => (
+          <LegendItem key={key}>
+            <LegendBox
+              style={{ backgroundColor: item.color, opacity: item.opacity }}
+            />
+            <LegendText>{item.name}</LegendText>
+          </LegendItem>
+        ))}
+      </Legend>
     </HomeContainer>
   );
 }
+
+HomePage.navigationOptions = ({ navigation }) => {
+  function ConfigButton() {
+    function handleBtnAction() {
+      navigation.navigate('HomeConfig');
+    }
+
+    return (
+      <ConfigButtonArea onPress={handleBtnAction}>
+        <ConfigButtonImage source={require('../../assets/config.png')} />
+      </ConfigButtonArea>
+    );
+  }
+
+  return {
+    title: 'Seu progresso diário',
+    headerRight: () => <ConfigButton />,
+    headerRightContainerStyle: {
+      marginRight: 10,
+    },
+  };
+};
+
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export const Home = connect(mapStateToProps, mapDispatchToProps)(HomePage);
